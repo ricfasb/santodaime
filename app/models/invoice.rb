211060983595979fcf0831166e042084aa97753a
Ricardo de Souza <ricardosouza@localhost.localdomain>
2019-08-12 23:47:05 -0300

    invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,16 @@
 class Invoice < ApplicationRecord
   belongs_to :invoice_type
   belongs_to :person
-
+  validate :min_amount
+  
   validates_presence_of :amount, :due_date, :description, :invoice_type, :person
     
+  def min_amount
+    if amount.nil? || amount == 0
+      errors.add(:amount, 'zerado não é permitido')
+    end
+  end
+
   def as_json(options={})    
     super(:only => [ :id, :due_date, :amount, :description ],
           :include => {
